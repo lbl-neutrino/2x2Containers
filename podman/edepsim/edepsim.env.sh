e481aea3983ede33b39401f3682de83593bc21f6
Matt Kramer <mjkramer@users.noreply.github.com>
2023-12-18 22:48:19 -0800

    Add edep-sim headers for CPATH
    So that convert2h5 doesn't fail with newer versions of ROOT.

--- podman/edepsim/edepsim.env.sh
+++ podman/edepsim/edepsim.env.sh
@@ -3,6 +3,9 @@
 export EDEPSIM="$GEN_DIR"/edep-sim/install
 export PATH="$EDEPSIM"/bin:${PATH}
 export LD_LIBRARY_PATH="$EDEPSIM"/lib:"$LD_LIBRARY_PATH"
+# After going from ROOT 6.14.06 to 6.28.06, apparently we need to point CPATH to
+# the edepsim-io headers. Otherwise convert2h5 fails.
+export CPATH=$EDEPSIM/include/EDepSim:$CPATH
 
 # This points to a MakeProject-generated library for the edep-sim output format
 # See comments in 2x2_sim's run_spill_build.sh
