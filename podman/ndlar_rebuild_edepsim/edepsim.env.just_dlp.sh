1f47c37a5f9d29ce8fccdc206cdaa82ff6cea478
Matt Kramer <mjkramer@users.noreply.github.com>
2025-12-07 07:02:22 -0800

    again

--- podman/ndlar_rebuild_edepsim/edepsim.env.just_dlp.sh
+++ podman/ndlar_rebuild_edepsim/edepsim.env.just_dlp.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# For libyaml-cpp (needed by MPV/MPR)
+export LD_LIBRARY_PATH=$EDEPSIM/lib64:$LD_LIBRARY_PATH
+
 # MPV/MPR "particle bomb" generator for SPINE training samples
 export DLPGENERATOR_INCDIR=$GEN_DIR/DLPGenerator/build/include
 export DLPGENERATOR_LIBDIR=$GEN_DIR/DLPGenerator/build/lib
